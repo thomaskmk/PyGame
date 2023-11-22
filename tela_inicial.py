5b4d47b57f0ef158f5278138b2c7455027ea7e4c
Maria Carolina Porto <mariacp9@al.insper.edu.br>
2023-11-22 08:11:31 -0300

    Apagar arquivos

--- tela_inicial.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-class inicio:
-    def __init__(self):
-        self.imagem = pygame.image.load('imagem/capa.png')
-        
-    def desenha(self, tela):
-        tela.blit(self.imagem, (0, 0))
