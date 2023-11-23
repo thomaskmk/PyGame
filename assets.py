ee7fcabb330beb8b0f45e41ab914a1ee149eb296
Maria Carolina Porto <mariacp9@al.insper.edu.br>
2023-11-23 12:49:45 -0300

    assets

---
+++ assets.py
@@ -0,0 +1,6 @@
+assets = {}
+assets["Background"] = "assets/background-day.png"
+assets["Base"] = "assets/base.png"
+assets["Background"] = "assets/bluebird-midflap.png"
+assets["Background"] = "assets/pipe-red.png"
+
