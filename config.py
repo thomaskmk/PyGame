b9595cb6c2d44ee6e4874cfdb2ba987bfd7114c9
Maria Carolina Porto <mariacp9@al.insper.edu.br>
2023-11-28 20:44:28 -0300

    Novas configuracoes

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from os import path
 from pygame.locals import *
 
 # Dados gerais do jogo.
-LARGURA = 400  # Largura da tela
-ALTURA = 700 # Altura da tela
+LARGURA = 1000  # Largura da tela
+ALTURA = 600 # Altura da tela
 FPS = 60 # Frames por segundo
 
 INIT = 0
@@ -17,16 +17,16 @@ LARG_CHAO = 2*LARGURA
 ALT_CHAO = 100
 
 # Dados gerais do cano:
-LARG_CANO = 80
-ALT_CANO = 500
-ESPACO_CANOS = 200
+LARG_CANO = 300
+ALT_CANO = 400
+ESPACO_CANOS = 70
 
 # Define algumas variáveis com as cores básicas
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+BLUE = (0, 0, 150)
 YELLOW = (255, 255, 0)
 
 
