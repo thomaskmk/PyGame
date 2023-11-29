e5d3bc61c6c391364678ef24226c3c52226fac3f
Maria Carolina Porto <mariacp9@al.insper.edu.br>
2023-11-28 22:37:12 -0300

    tiros

--- assets.py
+++ assets.py
@@ -14,3 +14,4 @@ assets["obstaculo"] = "assets/imagem/arvore1_pixel.png"
 assets['objetos'] = ['assets/imagem/objetos/bola8.png', 'assets/imagem/objetos/cadeira.png', 'assets/imagem/objetos/chave.png', 'assets/imagem/objetos/arrow.png', 'assets/imagem/objetos/foguete.png', 'assets/imagem/objetos/garrafa.png', 'assets/imagem/objetos/pneu.png', 'assets/imagem/objetos/sofa.png']
 assets['font'] = pygame.font.Font(None, 40)
 assets["background"] = "assets/imagem/ceu_pixels.png"
+assets["tiros"] = "assets/imagem/tiro_laser.png"
