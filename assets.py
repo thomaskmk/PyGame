079ba59e87cdd7090b71434adf3b853d0e92d847
Maria Carolina Porto <mariacp9@al.insper.edu.br>
2023-11-29 11:18:33 -0300

    novas imagens

--- assets.py
+++ assets.py
@@ -13,5 +13,5 @@ assets["aviao"] = "assets/imagem/aviao.png"
 assets["obstaculo"] = "assets/imagem/arvore1_pixel.png"
 assets['objetos'] = ['assets/imagem/objetos/bola8.png', 'assets/imagem/objetos/cadeira.png', 'assets/imagem/objetos/chave.png', 'assets/imagem/objetos/arrow.png', 'assets/imagem/objetos/foguete.png', 'assets/imagem/objetos/garrafa.png', 'assets/imagem/objetos/pneu.png', 'assets/imagem/objetos/sofa.png']
 assets['font'] = pygame.font.Font(None, 40)
-assets["background"] = "assets/imagem/ceu_pixels.png"
+assets["background"] = "assets/imagem/background.png"
 assets["tiros"] = "assets/imagem/tiro_laser.png"
