3d837e0cb866c538f28b2348cd5200ff1f0169b1
Thomas Kechichian Martins Kassabian <142352120+thomaskmk@users.noreply.github.com>
2023-11-29 11:50:13 -0300

    fix

--- jogo.py
+++ jogo.py
@@ -6,29 +6,36 @@ from tela_jogo import *
 from tela_game_over import*
 from assets import *
 
+# Inicia o pygame
 pygame.init()
 pygame.mixer.init()
 
+# Abre a janela
 window = pygame.display.set_mode((LARGURA, ALTURA))
 pygame.display.set_caption('Avião perdido')
 
+# Alterna os estados do jogo
 state = INIT
 while state != QUIT:
     if state == INIT:
+        # Toca a musica do inicio
         pygame.mixer.music.load('assets/sons/musica_de_fundo.mp3')
         pygame.mixer.music.set_volume(0.1)
         pygame.mixer.music.play(loops=-1)
+
         state = tela_inicial(window)
 
     if state == GAME:
         x = tela_jogo(window)
         state = x[0]
-        
+
     if state == GAME_OVER:
+        # Toca a musica da tela de game_over
         pygame.mixer.music.load('assets/sons/musica_game_over.mp3')
         pygame.mixer.music.set_volume(0.1)
         pygame.mixer.music.play(loops=-1)
         score = x[1]
+        
         state = tela_game_over(window, score)
 
 pygame.quit()
